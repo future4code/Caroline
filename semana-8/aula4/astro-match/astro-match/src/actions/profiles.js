57edf3e863ab03f5ebd4d867971393df2c58bde2
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-11-03 21:31:26 -0300

    tentando e errando, mas no caminho rs

--- semana-8/aula4/astro-match/astro-match/src/actions/profiles.js
+++ semana-8/aula4/astro-match/astro-match/src/actions/profiles.js
@@ -2,8 +2,32 @@ import axios from 'axios'
 
 export const clearSwipes = () => async (dispatch) => {
 	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/caroline/clear')
+
 }
 
+export const getProfileAction = (profiles) => ({
+	type: "GET_PROFILE_ACTION",
+	payload: {profiles}
+
+})
+
 export const getProfile = () => async (dispatch) => {
-	await axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/caroline/person')
-}
+	await axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/caroline/person').then((res) => {
+		dispatch(getProfileAction(
+			res.data.profiles.id,
+			res.data.profiles.age,
+			res.data.profiles.name,
+			res.data.profiles.bio,
+		))
+	}).catch((err) => {
+		console.log(err);
+	})
+};
+
+
+
+
+
+
+
+
--- semana-8/aula4/astro-match/astro-match/src/reducers/profiles.js
+++ semana-8/aula4/astro-match/astro-match/src/reducers/profiles.js
@@ -1,7 +1,17 @@
-const initialState = {}
+const initialState = {
+profiles: { },
+  
+}
 
 const profiles = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case "GET_PROFILE_ACTION":
+        return { ...state, profiles: action.payload.profiles };
+
+    default:
+        return state;
+  }
 }
+
 
 export default profiles
