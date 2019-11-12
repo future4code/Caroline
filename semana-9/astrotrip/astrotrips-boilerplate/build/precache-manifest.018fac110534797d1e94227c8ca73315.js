be15b9de93e589dc4d42409727a624a6e7e1249c
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-11-12 09:53:56 -0300

    trocar master

---
+++ semana-9/astrotrip/astrotrips-boilerplate/build/precache-manifest.018fac110534797d1e94227c8ca73315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156aa44d5a061a6c9ed16ee2dc4144a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0445ab08aa5fe34b8997",
+    "url": "/static/js/2.e824c09f.chunk.js"
+  },
+  {
+    "revision": "9bae422bfab50aa90b38",
+    "url": "/static/js/main.c733c0a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22bc2104cd403f16368c37f5e1793de7",
+    "url": "/static/media/logo.22bc2104.png"
+  },
+  {
+    "revision": "74d38366c3507c800a4aaed0dddf73e1",
+    "url": "/static/media/rocket.74d38366.jpg"
+  }
+]);
--- semana-9/astrotrip/astrotrips-boilerplate/build/service-worker.js
+++ semana-9/astrotrip/astrotrips-boilerplate/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.850c7d1d96c4f66ad75ffaf7966f5272.js"
+  "/precache-manifest.018fac110534797d1e94227c8ca73315.js"
 );
 
 self.addEventListener('message', (event) => {
