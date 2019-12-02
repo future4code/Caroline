4bec141563081fed4ab010a4f695aedaf33a70de
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-12-02 12:22:59 -0300

    colocando em string

--- semana-13/aula1node/myCalculator.js
+++ semana-13/aula1node/myCalculator.js
@@ -3,18 +3,18 @@ const value1 = Number(process.argv[3]);
 const value2 = Number(process.argv[4]);
 
 if (op === "add"){
-	console.log( value1 + value2);
+	console.log( `Resposta: ${value1 + value2}`);
 } 
 
 else if (op === "sub"){
-	console.log( value1 - value2);
+	console.log( `Resposta: ${value1 - value2}`);
 } 
 
 else if (op === "mult"){
-	console.log( value1 * value2);
+	console.log( `Resposta: ${value1 * value2}`);
 } 
 
 else if (op === "div"){
-	console.log( value1 / value2);
+	console.log( `Resposta: ${value1 / value2}`));
 } 
 
