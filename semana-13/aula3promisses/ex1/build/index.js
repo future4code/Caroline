965afe71df480da74854e0370433a903882ba704
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-12-04 15:20:26 -0300

    mostrar 1 texto ok

--- semana-13/aula3promisses/ex1/build/index.js
+++ semana-13/aula3promisses/ex1/build/index.js
@@ -1,2 +1,21 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const fileName = '1.txt';
+const readFilePromise = new Promise((resolve, reject) => {
+    fs_1.readFile(fileName, (err, data) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        const fileContent = data.toString();
+        resolve(fileContent);
+    });
+});
+readFilePromise.then((content) => {
+    console.log("Este é o conteúdo do arquivo", content);
+}).catch(e => {
+    console.error("Opa! Deu erro na Promise", e);
+});
+console.log("fileName");
 //# sourceMappingURL=index.js.map
--- semana-13/aula3promisses/ex1/src/index.ts
+++ semana-13/aula3promisses/ex1/src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello")
+import { readFile } from 'fs';
+
+const fileName:string = '1.txt';
+
+const readFilePromise = new Promise((resolve, reject) => {
+  readFile(fileName, (err, data:Buffer) => {
+    if(err){
+      reject(err);
+      return;
+    }
+
+    const fileContent = data.toString();
+    resolve(fileContent)
+  });
+});
+
+readFilePromise.then((content) => {
+  console.log("Este é o conteúdo do arquivo", content)
+}).catch(e => {
+  console.error("Opa! Deu erro na Promise", e)
+});
+
+
+console.log("fileName")
+
