a8472ef179bf29d53a3fd69d7baa71cc6b2db9de
Caroline Lopes <caroline.lopesm@hotmail.com>
2020-01-14 08:58:24 -0300

    iniciando bcrypt

--- semana-17/Aula1JWT/src/services/bcrypt.ts
+++ semana-17/Aula1JWT/src/services/bcrypt.ts
@@ -1,1 +1,9 @@
 import * as bcrypt from "bcrypt"
+
+const encripto = async () => {
+    const generateSalt = await bcrypt.genSalt(10);
+    const password = "123456"
+    const result = await bcrypt.hash(password,generateSalt );
+    console.log("Senha codigicada: ", result)
+    
+}
