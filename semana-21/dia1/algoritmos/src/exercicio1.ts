1d7251b652142c9e9209f09da1364edfb29eeb61
Caroline Lopes <caroline.lopesm@hotmail.com>
2020-02-19 07:48:55 -0300

    dia 1 e 2

---
+++ semana-21/dia1/algoritmos/src/exercicio1.ts
@@ -0,0 +1,11 @@
+export const wordRepeated = (string: string) => {
+  const inputs =string
+  for(let i = 0; i < inputs.length; i++) 
+  for (const word of inputs) {
+   
+ return inputs
+};
+
+console.log(wordRepeated("blablaa"))
+
+}
